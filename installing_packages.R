67c22f51f9cf6ffb64558dbc33280061fcc5fb75
Amin Shoari Nejad <aminsh2010@gmail.com>
2021-04-03 16:25:18 +0100

    Update installing_packages.R

--- installing_packages.R
+++ installing_packages.R
@@ -4,10 +4,6 @@
 #)
 
 install.packages(
-  c("dplyr", "lubridate", "tidyverse", "reshape2", "ggplot2", 
-    "rgdal", "tigris", "spdplyr",
-    "ggdendro", "scales", "RcppRoll","zoo", "readxl", "Jmisc", 
-    "cowplot", "survival", "devtools", "git2r"
-  ),
+  c("dplyr", "lubridate", "tidyr", "purrr", "zoo", "readr"),
   repos = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"
 )
