3917d44c5a9a46d8899e6c4b584d1875a5c8dd13
Amin Shoari Nejad <aminsh2010@gmail.com>
2021-04-03 15:04:09 +0100

    Update installing_packages.R

--- installing_packages.R
+++ installing_packages.R
@@ -1,4 +1,4 @@
 install.packages(
-  c("lubridate", "tidyverse", "RcppRoll","zoo", "reticulate"),
+  c("lubridate", "tidyverse", "RcppRoll","zoo"),
   repos = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"
 )
