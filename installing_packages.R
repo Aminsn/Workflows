305edaaa62be6d115eb39f151f16a7d034934f61
Amin Shoari Nejad <aminsh2010@gmail.com>
2021-04-03 16:04:45 +0100

    Update installing_packages.R

--- installing_packages.R
+++ installing_packages.R
@@ -1,4 +1,13 @@
+#install.packages(
+#  c("lubridate", "stringi", "tidyverse", "RcppRoll","zoo"),
+#  repos = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"
+#)
+
 install.packages(
-  c("lubridate", "stringi", "tidyverse", "RcppRoll","zoo"),
+  c("dplyr", "lubridate", "tidyverse", "reshape2", "ggplot2", 
+    "rgdal", "tigris", "spdplyr",
+    "ggdendro", "scales", "RcppRoll","zoo", "readxl", "Jmisc", 
+    "cowplot", "survival", "devtools", "git2r"
+  ),
   repos = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"
 )
